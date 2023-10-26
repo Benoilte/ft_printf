b235c90bfe26dbb7df6406551da1f47f72891a78
benoit brandt <benoit.07@hotmail.com>
2023-10-26 22:49:30 +0200

    update main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:47:48 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/26 22:45:15 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/26 22:49:00 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		main(void)
 {
 	int	count;
 
-	count = ft_printf("Hello%c %dWorld!\n", 32, 24);
+	count = ft_printf("Hello%c %dWorld!\n", 33, 24);
 	printf("count: %d\n", count);
 	printf("[%10.2s]\n", "hello");
 	printf("[%+04d]\n", 42);
