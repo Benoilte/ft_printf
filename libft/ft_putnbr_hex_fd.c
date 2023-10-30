d8a14449dba84b8d4536e54987e2e3683e4c032a
Bebrandt <benoit.07@hotmail.com>
2023-10-30 11:07:26 +0100

    modify argument name of unsigned int

--- libft/ft_putnbr_hex_fd.c
+++ libft/ft_putnbr_hex_fd.c
@@ -6,7 +6,7 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 09:04:16 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/30 10:58:11 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/30 11:02:41 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 Convert the unsigned int 'n' into its corresponding hex nbr 
 and writes it on the file descriptor 'fd'
 */
-void	ft_putnbr_hex_fd(unsigned int n, int fd)
+void	ft_putnbr_hex_fd(unsigned int uint, int fd)
 {
 	char	*str_hex;
 
 	str_hex = "0123456789abcdef";
-	if (n > 15)
-		ft_putnbr_hex_fd((n / 16), fd);
-	ft_putchar_fd(str_hex[n % 16], fd);
+	if (uint > 15)
+		ft_putnbr_hex_fd((uint / 16), fd);
+	ft_putchar_fd(str_hex[uint % 16], fd);
 }
