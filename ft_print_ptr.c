b5a106f54c3ba6aad9183ef94d6fe793da6f84bf
Bebrandt <benoit.07@hotmail.com>
2023-10-31 10:10:40 +0100

    take consideration of null pointer

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -6,22 +6,34 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 21:21:25 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/10/30 20:33:16 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/10/31 10:09:45 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void ft_set_ptr_lst(t_printf_lst *lst);
 
 int	ft_print_ptr(void *p, t_printf_lst *lst)
 {
 	uintptr_t	ptr;
 	char		*hex_nbr;
 
+	if (!p)
+	{
+		ft_putstr_fd("0x0", 1);
+		return (3);
+	}
+	ft_set_ptr_lst(lst);
+	ptr = (uintptr_t)p;
+	hex_nbr = ft_itoa_hex(ptr);
+	return (ft_print_hex_format(hex_nbr, lst));
+}
+
+static void ft_set_ptr_lst(t_printf_lst *lst)
+{
 	lst->flag_hash = 1;
 	lst->flag_zero = 0;
 	lst->precision = -1;
 	lst->specifier = 'x';
-	ptr = (uintptr_t)p;
-	hex_nbr = ft_itoa_hex(ptr);
-	return (ft_print_hex_format(hex_nbr, lst));
 }
